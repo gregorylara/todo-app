9df8cc172c144583d791212f6555dea836b3ea86
Gregory Lara <gregorylara@outlook.com>
2026-01-08 20:56:57 +0100

    update todo store

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -17,8 +17,69 @@ const state = {
 }
 
 const initStore = () => {
-    console.log('InitStore 🥑');
     
-}
+    console.log(state);
+    console.log('InitStore 👜');
+    }
+    const getTodos = ( filter = Filters.All ) => {
 
+        switch (filter) {
+            
+            case Filters.All:
+            return [...state.todos];
+            
+            case Filters.Completed:
+            return state.todos.filter( todo => todo.done);
+            
+            case Filters.Pending:
+            return state.todos.filter( todo => !todo.done);
+            
+            default:
+            throw new Error(`Option ${ filter } is not valid`);   
+
+        }
+
+    }
+    const loadStore = () => {
+        throw new Error('Not implemented');
+    }
+    const addTodo = ( description ) => {
+        if ( !description ) throw new Error('Description is required');
+        state.todos.push( new Todo(description) );
+    }
+    const toggleTodo = ( todoId ) => {
+        
+        state.todos = state.todos.map( todo => {
+            if ( todo.id === todoId ){
+                todo.done = !todo.done;
+            }
+            return todo;
+        })
+    }
+    const deleteTodo = ( todoId ) => {
+        state.todos = state.todos.filter( todo => todo.id !== todoId);
+    }
+    const deleteCompleted = ( todoId ) => {
+        state.todos = state.todos.filter( todo => todo.done);
+    }
+    const setFilter = ( newFilter = Filters.All ) => {
+        state.filter = newFilter;
+    }
+    const getCurrentFilter = () => {
+        return state.filter;
+    }
+
+
+
+export default{
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
+    getTodos,
+    initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
+}
 
--- src/todos/app.js
+++ src/todos/app.js
@@ -1,4 +1,4 @@
-
+import html from './app.html?raw'
 /**
  * 
  * @param {String} elementId 
@@ -10,7 +10,7 @@ export const App = ( elementId ) => {
     (()=> {
 
         const app = document.createElement('div');
-        app.innerHTML = '<h1>Hola Mundo</h1>';
+        app.innerHTML = html;
         document.querySelector(elementId).append( app );
 
     })();
